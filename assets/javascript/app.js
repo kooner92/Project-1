fdafa7c7fa93f5f50a8b0a1bd69ab66a745aaddc
Justin Kooner <kooner92@gmail.com>
2018-09-07 12:37:06 -0700

    added google

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,21 @@
 $(document).ready(function () {
 
-function displaySearch() {
-    var apiKey = "AIzaSyDrpfdCExJEExJV2qeTvUosEud5Issbyps";
-    var proxy = "http://cors-anywhere.herokuapp.com/"
-    var queryURL = `${proxy}https://maps.googleapis.com/maps/api/directions/json?origin=4603+Crosshaven+Bakersfield+CA&destination=Fresno`;
-    console.log(apiKey);
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        var results = response;
-        console.log(results);
-    });
-}
+    // function displaySearch() {
+    //     var string = "218 packing pl, ca"
 
-displaySearch();
+    //     var proxy = "http://cors-anywhere.herokuapp.com/"
+    //     var queryURL = `${proxy}https://maps.googleapis.com/maps/api/geocode/json?address=${string}&key=`;    
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //     }).then(function (response) {
+    //         console.log(response);
+    //         console.log(response.results[0].geometry.location.lat);
+    //         console.log(response.results[0].geometry.location.lng);
+    //     });
+
+
+    // displaySearch();
+
 
 });
