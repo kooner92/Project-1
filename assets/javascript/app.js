3b2fd08a9d8ab94daff25f18839e65692becbf65
Justin Kooner <kooner92@gmail.com>
2018-09-07 13:46:11 -0700

    delete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,20 @@
 $(document).ready(function () {
 
-    // function displaySearch() {
-    //     var string = "218 packing pl, ca"
+    function displaySearch() {
+        var string = "westwood, ca"
 
-    //     var proxy = "http://cors-anywhere.herokuapp.com/"
-    //     var queryURL = `${proxy}https://maps.googleapis.com/maps/api/geocode/json?address=${string}&key=`;    
-    //     $.ajax({
-    //         url: queryURL,
-    //         method: "GET"
-    //     }).then(function (response) {
-    //         console.log(response);
-    //         console.log(response.results[0].geometry.location.lat);
-    //         console.log(response.results[0].geometry.location.lng);
-    //     });
-
-
-    // displaySearch();
-
+        var proxy = "http://cors-anywhere.herokuapp.com/"
+        var queryURL = `${proxy}https://maps.googleapis.com/maps/api/geocode/json?address=${string}&key=`;    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            console.log(response.results[0].geometry.location.lat);
+            console.log(response.results[0].geometry.location.lng);
+        });
+    
+    }
+        displaySearch();
 
 });
