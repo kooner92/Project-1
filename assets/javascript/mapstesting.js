b07a79f71ea484ddb901c8c5457243b73472303d
lookrumad <verniedelacruz@gmail.com>
2018-09-11 23:43:34 -0700

    progressing with time as a factor

--- assets/javascript/mapstesting.js
+++ assets/javascript/mapstesting.js
@@ -38,25 +38,38 @@ $(document).ready(function () {
         var apiKey = "AIzaSyDMm86-L51560jHqvvQ46cAZGTyOtYvlT4";
         var proxy = "https://cors-anywhere.herokuapp.com/";
         var queryURL = `${proxy}https://maps.googleapis.com/maps/api/directions/json?origin=${address1}&destination=${address2}&key=${apiKey}`;
-        
+
         $("#loadingModal").modal({
             dismissible: false
-         });
-         if (address1 !== "" && address2 !== "") {
-         $("#loadingModal").modal("open");
-         };
+        });
+        if (address1 !== "" && address2 !== "") {
+            $("#loadingModal").modal("open");
+        };
 
         if (address1 !== "" && address2 !== "") {
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
-                console.log(response);
                 if (response.status === "NOT_FOUND") {
                     $("#loadingModal").modal("close");
                     $("#modal2").modal("open");
                 }
                 else {
+                    console.log(response.routes[0]);
+                    var duration = parseInt(response.routes[0].legs[0].duration.text) / 2;
+                    var stepTime = parseInt(response.routes[0].legs[0].steps[0].duration.text);
+                    var j = 0;
+                    console.log(duration);
+                    console.log(parseInt(stepTime));
+                    while (stepTime < duration) {
+                        j++;
+                        stepTime += parseInt(response.routes[0].legs[0].steps[j].duration.text);
+                        console.log(j);
+                        console.log(stepTime);
+                    }
+                    console.log("lat" + response.routes[0].legs[0].steps[j].end_location.lat);
+                    console.log("lng" + response.routes[0].legs[0].steps[j].end_location.lng);
                     var latitude1 = response.routes[0].legs[0].start_location.lat;
                     var longitude1 = response.routes[0].legs[0].start_location.lng;
                     var latitude2 = response.routes[0].legs[0].end_location.lat;
@@ -89,46 +102,13 @@ $(document).ready(function () {
                     if (yelpAttributes !== undefined) {
                         advanceSearch = "&attributes=" + yelpAttributes.toString();
                     }
-                    else {
-                        var latitude1 = response.routes[0].legs[0].start_location.lat;
-                        var longitude1 = response.routes[0].legs[0].start_location.lng;
-                        var latitude2 = response.routes[0].legs[0].end_location.lat;
-                        var longitude2 = response.routes[0].legs[0].end_location.lng;
-                        var midLongitude = "&longitude=" + ((longitude1 + longitude2) / 2);
-                        var midLatitude = "&latitude=" + ((latitude1 + latitude2) / 2);
-                        var term = "";
-                        var price = $("input[name='price']:checked").val();
-                        var radius = $("input[name='radius']:checked").val();
-                        var sortBy = $("input[name='sortBy']:checked").val();
-                        var open = "";
-                        var yelpAttributes = [];
-                        var advanceSearch = "";
-
-                        if ($("#term").val() !== "") {
-                            term = "&term=" + $("#term").val();
-                        }
-                        if ($("input[id='openNow']:checked").val() !== undefined) {
-                            open = $("input[id='openNow']:checked").val();
-                        }
-                        if ($("input[id='hotNew']:checked").val() !== undefined) {
-                            yelpAttributes.push($("input[id='hotNew']:checked").val());
-                        }
-                        if ($("input[id='reservation']:checked").val() !== undefined) {
-                            yelpAttributes.push($("input[id='reservation']:checked").val());
-                        }
-                        if ($("input[id='deals']:checked").val() !== undefined) {
-                            yelpAttributes.push($("input[id='deals']:checked").val());
-                        }
-                        if (yelpAttributes !== undefined) {
-                            advanceSearch = "&attributes=" + yelpAttributes.toString();
-                        }
-                    }
                 }
                 $.ajax({
                     url: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?text=del&limit=5${midLongitude}${midLatitude}${term}${price}${radius}${sortBy}${open}${advanceSearch}`,
                     method: "GET",
                     headers: { "Authorization": "Bearer 0xtZshssd9WzNzqiXrck1pdz-jC9mbOOkdDQL6xxKj9g78FU9wRHpXKxGLLSNAVo2jR-0bcLCaUn9x9yj8zGbBVY2zUM6wnl6-rWjmAo2mdtG_LSaF-uS7dDPLaQW3Yx" }
                 }).then(function (response) {
+                    console.log(response);
                     if (response.businesses.length === 0) {
                         $("#modal1").modal("open");
                         $("#loadingModal").modal("close");
